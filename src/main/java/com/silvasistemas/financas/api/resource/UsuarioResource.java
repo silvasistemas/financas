23a2d5558f3d0c1c69514d1e7794187711165192
Ricardo Silva <silvasistemas@gmail.com>
2023-02-13 21:30:04 -0300

    Concluindo o controller de Lancamentos

--- src/main/java/com/silvasistemas/financas/api/resource/UsuarioResource.java
+++ src/main/java/com/silvasistemas/financas/api/resource/UsuarioResource.java
@@ -1,7 +1,12 @@
 package com.silvasistemas.financas.api.resource;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +16,19 @@ import com.silvasistemas.financas.api.dto.UsuarioDTO;
 import com.silvasistemas.financas.exception.ErroAutenticacao;
 import com.silvasistemas.financas.exception.RegraNegocioException;
 import com.silvasistemas.financas.model.entity.Usuario;
+import com.silvasistemas.financas.service.LancamentoService;
 import com.silvasistemas.financas.service.UsuarioService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
 public class UsuarioResource {
 	
-	private UsuarioService service;
-	
-	public UsuarioResource(UsuarioService service) {
-		this.service = service;
-	}
+	private final UsuarioService service;
+	private final LancamentoService lancamentoService;
+
 	
 	@PostMapping("/autenticar")
 	public ResponseEntity<?> autenticar( @RequestBody UsuarioDTO dto ) {
@@ -49,6 +56,18 @@ public class UsuarioResource {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 		
+	}
+	
+	@GetMapping("{id}/saldo")
+	public ResponseEntity obterSaldo( @PathVariable("id") Long id ) {
+		Optional<Usuario> usuario = service.obterPorId(id);
+		
+		if(!usuario.isPresent()) {
+			return new ResponseEntity( HttpStatus.NOT_FOUND );
+		}
+		
+		BigDecimal saldo = lancamentoService.obterSaldoPorUsuario(id);
+		return ResponseEntity.ok(saldo);
 	}
 	
 }
--- src/main/java/com/silvasistemas/financas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/silvasistemas/financas/service/impl/UsuarioServiceImpl.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.silvasistemas.financas.exception.ErroAutenticacao;
@@ -16,12 +16,15 @@ import com.silvasistemas.financas.service.UsuarioService;
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
 
-	@Autowired
 	private UsuarioRepository repository;
-
-	public UsuarioServiceImpl(UsuarioRepository repository) {
+	private PasswordEncoder encoder;
+	
+	public UsuarioServiceImpl(
+			UsuarioRepository repository, 
+			PasswordEncoder encoder) {
 		super();
 		this.repository = repository;
+		this.encoder = encoder;
 	}
 
 	@Override
@@ -32,10 +35,12 @@ public class UsuarioServiceImpl implements UsuarioService {
 			throw new ErroAutenticacao("Usuário não encontrado para o email informado.");
 		}
 		
-		if(!usuario.get().getSenha().equals(senha)) {
+		boolean senhasBatem = encoder.matches(senha, usuario.get().getSenha());
+		
+		if(!senhasBatem) {
 			throw new ErroAutenticacao("Senha inválida.");
 		}
-		
+
 		return usuario.get();
 	}
 
@@ -43,16 +48,28 @@ public class UsuarioServiceImpl implements UsuarioService {
 	@Transactional
 	public Usuario salvarUsuario(Usuario usuario) {
 		validarEmail(usuario.getEmail());
+		criptografarSenha(usuario);
 		return repository.save(usuario);
+	}
+
+	private void criptografarSenha(Usuario usuario) {
+		String senha = usuario.getSenha();
+		String senhaCripto = encoder.encode(senha);
+		usuario.setSenha(senhaCripto);
 	}
 
 	@Override
 	public void validarEmail(String email) {
 		boolean existe = repository.existsByEmail(email);
-		if (existe) {
+		if(existe) {
 			throw new RegraNegocioException("Já existe um usuário cadastrado com este email.");
 		}
-
 	}
+
+	@Override
+	public Optional<Usuario> obterPorId(Long id) {
+		return repository.findById(id);
+	}
+
 
 }
